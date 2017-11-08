740c90883e9eada6aa0edb99796aad6ffc1be2ee
eduhlira4 <ejlas.lira@gmail.com>
2017-11-08 10:28:15 -0400

    Primeira Versão para o Vídeo

---
+++ DabukuriProject/Assets/Script/AlterarObjeto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlterarObjeto : MonoBehaviour {
+
+	public Image objeto, nomeTrocar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TrocarImagem(Sprite Objeto){
+
+		objeto.sprite = Objeto;
+	}
+	public void TrocarNome (Sprite Nome){
+		nomeTrocar.sprite = Nome;
+	}
+}
---
+++ DabukuriProject/Assets/Script/AutoSkip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoSkip : MonoBehaviour {
+
+	public int timeToSkip;
+	public string scene;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SkipScene", timeToSkip);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SkipScene(){
+		
+		SceneManager.LoadScene (scene);
+	}
+}
