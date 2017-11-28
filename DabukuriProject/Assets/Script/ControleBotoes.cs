1e34863361a8364aa98221ccd6ded22f28b2ab30
eduhlira4 <ejlas.lira@gmail.com>
2017-11-27 22:35:38 -0400

    Fase Semi concluida

---
+++ DabukuriProject/Assets/Script/ControleBotoes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleBotoes : MonoBehaviour {
+
+    public GameObject[] imagens;
+	public int index;
+	public GameObject letraAtiva;
+
+	// Use this for initialization
+	void Start () {
+		
+		letraAtiva = imagens[index];
+		trocarImagens();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void trocarImagens()
+    {
+		letraAtiva.SetActive(false);
+		letraAtiva = imagens[index%imagens.Length];
+		letraAtiva.SetActive(true);
+    }
+
+	public void percorrerIndex(int n){
+		index+=n;
+		trocarImagens();
+	}
+		
+}
